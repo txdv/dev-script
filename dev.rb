03adc4d3b54f6eeffb511b9f14d8cdd423761bc1
Andrius Bentkus <andrius.bentkus@gmail.com>
2016-08-01 10:23:40 +0300

    Simplify colorization, use colorize.
    Now a string can be colorized by just calling .red, .yellow, etc.
    Sending a message is also possible.

--- dev.rb
+++ dev.rb
@@ -1,7 +1,25 @@
 #!/usr/bin/env ruby
 
 require 'active_support/core_ext/hash/keys'
+require 'colorize'
 require 'yaml'
+
+# override https://github.com/fazibear/colorize/blob/8af1ec5e8223a8d7c59d29f08552fb2e28ed8202/lib/colorize/instance_methods.rb#L19
+# The PS1 variable expects an escaped version.
+
+module Colorize
+  module InstanceMethods
+    def colorize(params)
+      return self if self.class.disable_colorization
+      require_windows_libs
+      scan_for_colors.inject(self.class.new) do |str, match|
+        colors_from_params(match, params)
+        defaults_colors(match)
+        str << "\\[\\033[#{match[0]};#{match[1]};#{match[2]}m#{match[3]}\\]\\033[0m\\]"
+      end
+    end
+  end
+end
 
 class ColorHash < Hash
   def initialize(hash)
@@ -72,23 +90,18 @@ color = esc.color
 
 apps = YAML::load_file('dev.yaml').map(&:symbolize_keys)
 
-def colorize(normal, accent, symbols, str)
-  return accent + str.gsub(/[#{symbols}]/) { |arg| normal + arg + accent }
-end
-
-
-pre = "#{color.lightgreen}#{esc.username}#{color.white}@#{color.purple}#{esc.host}#{color.white}:";
-
 str = apps.collect do |app|
   name = app[:name]
   which = `which #{app[:exec]}`
   next if which.length == 0
   next if which.start_with?("/usr/bin/")
   version = `#{app[:version]}`.strip
-  colorize(color.white, color[app[:color].to_sym], "\.", "#{name}-#{version}")
-end.reject { |x| x == nil }.join("#{color.white}:")
+  "#{name}-#{version}".gsub(/[\w]+/) { |s| s.send(app[:color]) }
+end.reject(&:nil?).join(':')
 
-res = pre + str + "#{color.white}: #{color.blue}#{esc.pwd} #{color.lightgreen}$#{color.nc} "
+str += ':' if str.length > 0
+
+res = "#{esc.username.light_green}@#{esc.host.magenta}:#{str} #{esc.pwd.blue} $ "
 
 vars = { PS1: res }
 vars.each do |key, value|
